46e5891cbd79999f23e8e20e1c36773215fde603
Roselou <elizabethroselou@gmail.com>
2018-04-03 12:10:26 -0700

    setup folders and directories

---
+++ seed.js
@@ -0,0 +1,91 @@
+//hard-coded data for fav artists 
+
+//SCHEMA OUTLINE
+var favArtistsSchema = new Schema {
+	name: String, 
+	movement: String, 
+	//medium: String,
+	//artworkTitle: String,
+	//artworkYear: Number,
+	//themes: String
+
+
+}
+
+//HARD-CODED DATABASE INFO (15 total)
+
+var favArtists = {
+	name: 'Hito Steyerl',
+	movement: 'Contemporary'
+	medium: 'Film and Writing'
+}, 
+{
+	name: 'Alina Szapocznicow', 
+	movement: 'Surrealism',
+	medium: 'Sculpture'
+},
+{
+	name: 'Yoko Ono',
+	movement: 'Fluxus',
+	medium: 'Performance Art and Film'
+},
+{
+	name: 'Shigeko Kubota', 
+	movement: 'Fluxus',
+	medium: 'Video Art and Sculpture Video'
+}, 
+{
+	name: 'Pipilotti Rist', 
+	movement: 'Contemporary',
+	medium: 'Video Installation'
+}, 
+{
+	name: 'Alicja Kwade', 
+	movement: 'Contemporary',
+	medium: 'Sculpture'
+}, 
+{
+	name: 'Maya Deren', 
+	movement: 'Surrealism',
+	medium: 'Experimental Film'
+}, 
+{
+	name: 'Monir Shahroudy', 
+	movement: 'Geometric Minimalism and Saqqakhaneh',
+	medium: 'Multimedia (drawings, mirrored mosaics)'
+}, 
+{
+	name: 'Lygia Clarke',
+	movement: 'Brazilian Constructivism',
+	medium: 'Installation, Painting, Wearable Sculptures'
+},
+{
+	name: 'Charlotte Moorman',
+	movement: 'Fluxus',
+	medium: 'Performance Art'
+}, 
+{
+	name: 'Eva Hesse',
+	movement: 'Minimalism',
+	medium: 'Sculpture'
+},
+{
+	name: 'Kara Walker',
+	movement: 'Contemporary',
+	medium: 'Multimedia (Painting, Installation, filmmaker'
+},
+{
+	name: 'Mierle Laderman Ukeles',
+	movement: 'Feminist Art',
+	medium: 'Performance Art'
+}, 
+{
+	name: 'Sophie Calle',
+	movement: 'Contemporary Art',
+	medium: 'Photography'
+}, 
+{
+	name: 'Louise Bourgeois',
+	movement: 'Surrealism'
+	medium: 'Sculpture and Installation Art'
+}
